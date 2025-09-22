0b50cce42fe7e02016545599d51598a11906ceed
arditbytyci <ab45425@ubt-uni.net>
2025-09-22 13:44:16 +0200

    feat(FavoriteDishes): Implement motion effects and refactor structure
    Enhances the FavoriteDishes component by integrating framer-motion 
    for animated effects and refactoring the layout structure. 
    
    Replaces static elements with dynamic DishArticle components to 
    improve code organization and maintainability.

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,20 @@
 
 import { Hero } from "@/components/Hero";
 import { FavoriteDishes } from "@/components/FavoriteDishes";
+import {motion, useMotionValueEvent, useScroll, useSpring, useTransform} from 'framer-motion'
+import { useRef } from "react";
+import test from "node:test";
 
 export default function Home() {
+
+  
+
+
+  
+
+
   return (
-    
+        
       <main data-scroll-container className="flex flex-col">
         <section className="h-screen w-full">
           <Hero />
--- components/FavoriteDishes/index.tsx
+++ components/FavoriteDishes/index.tsx
@@ -1,15 +1,30 @@
 import Image from "next/image"
+import {motion, useInView, useMotionValueEvent, useScroll } from 'framer-motion'
+import { useEffect, useRef } from "react";
+import { DishArticle } from "./DishArticle";
+
+
+// initial={{opacity:0,y:0}} whileInView={{opacity:1, y:0}} 
+//         transition={{duration: 0.5,ease: "easeInOut"}} viewport={{amount:0.3}}
+
 
 
 export  const FavoriteDishes = () => {
+
+
   return (
-<div className="w-full h-full grid grid-cols-2">
-  <div className="flex flex-col items-center justify-evenly"><Image src="/hero.png" width={250} height={250} alt="Plate" /> <p className="text-2xl text-[#65171D] font-bold">Tave Kosi</p></div>
-  <div className="bg-[#65171D] flex flex-col  items-center justify-evenly"><Image src="/hero.png" width={250} height={250} alt="Plate" /> <p className="text-2xl text-[#FFEEDF] font-bold">Tave Kosi</p></div>
-  <div className="flex flex-col  items-center justify-evenly"><Image src="/hero.png" width={250} height={250} alt="Plate" /> <p className="text-2xl text-[#65171D] font-bold">Tave Kosi</p></div>
-  <div className="bg-[#65171D] flex flex-col  items-center justify-evenly"><Image src="/hero.png" width={250} height={250} alt="Plate" /> <p className="text-2xl text-[#FFEEDF] font-bold">Tave Kosi</p></div>
-   
-</div>
+<motion.div className="w-full h-full grid grid-cols-2">
+   <div className="grid grid-cols-1">
+    <DishArticle name="Tave Kosi" textColor="#65171D" />
+    <DishArticle name="Tave Kosi" textColor="#65171D" />
+   </div>
+   <div className="bg-[#65171d] grid grid-cols-1">
+    <DishArticle name="Tave Kosi" textColor="#FFEEDF" />
+    <DishArticle name="Tave Kosi" textColor="#FFEEDF" />
+   </div>
+
+
+</motion.div>
   )
 }
 
